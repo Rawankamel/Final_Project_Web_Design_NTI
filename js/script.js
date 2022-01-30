d2b7ae43477bc41548f96c6428fdb7327e02cf4c
Rawan Kammel <rawan.kamel12@gmail.com>
2022-01-30 13:50:05 +0200

    Adding verfication

--- js/script.js
+++ js/script.js
@@ -23,3 +23,85 @@ $(document).ready(function() {
     });
 
 });
+
+var topBtn = document.getElementById("topBtn")
+var main_header = document.getElementById("main_header")
+var main_header_Style = main_header.style;
+window.addEventListener("scroll", function() {
+
+    if (document.documentElement.scrollTop > 400) {
+
+        main_header_Style.position = "sticky"
+        main_header_Style.top = "0"
+        main_header_Style.backgroundColor = "white"
+        main_header_Style.transition = "top 0.2s ease-in"
+        topBtn.style.display = "block"
+
+
+    } else topBtn.style.display = "none"
+})
+
+
+var form = document.getElementById("myForm")
+
+form.addEventListener("input", function(e) {
+    switch (e.target.id) {
+        case "inputName":
+            nameValidation(e.target)
+            break
+        case "inputEmail":
+            emailValidation(e.target)
+            break
+        case "inputPassword":
+            passwordValidation(e.target)
+
+            break
+    }
+})
+
+
+function nameValidation(input) {
+    var inputValue = input.value.trim()
+    if (inputValue.length < 3) handleError(input, "enter at least 3 character")
+    else handleError(input)
+}
+
+
+function emailValidation(input) {
+
+    var regExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    var inputValue = input.value
+
+    console.log(regExp.test(inputValue))
+
+    if (regExp.test(inputValue)) handleError(input)
+    else handleError(input, "enter a valid email")
+
+}
+
+function passwordValidation(input) {
+    var regExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
+    var inputValue = input.value
+    console.log(regExp.test(inputValue))
+    if (regExp.test(inputValue)) handleError(input)
+    else handleError(input, "please enter a complex password")
+}
+
+
+function handleError(element, msg = "") {
+    element.nextElementSibling.innerText = msg
+}
+
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault()
+})
+
+var form_control = document.getElementsByClassName("form-control");
+var p = document.getElementById("verfied_class_P")
+
+function verfied(this) {
+    for (var i = 0; i <= 2; i++) {
+
+    }
+}
